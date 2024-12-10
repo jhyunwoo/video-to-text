5860a821b2973d068ddfd03b66157a98f0c32547
jhyunwoo <jhyunwoo0228@gmail.com>
2024-12-10 15:16:15 +0900

    add 기세문

--- main.py
+++ main.py
@@ -2,52 +2,81 @@ import subprocess
 
 video_data = [
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/9a8bb5a4-b2b5-4722-9ede-8b7df826ce67/mp4/9a8bb5a4-b2b5-4722-9ede-8b7df826ce67.mp4/index.m3u8",
-        "title": "재산거래와법_7주차",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/f3c04906-4ac7-419e-898b-cbec3940d5fb/mp4/f3c04906-4ac7-419e-898b-cbec3940d5fb.mp4/index.m3u8",
+        "title": "기독교와세계문화_9주차_1",
     },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/9c46869c-3941-449b-b13d-2f8675cb47c3/mp4/9c46869c-3941-449b-b13d-2f8675cb47c3.mp4/index.m3u8",
-        "title": "재산거래와법_9주차",
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/5dbeb766-8337-42a9-9d9e-83fd359be35f/mp4/5dbeb766-8337-42a9-9d9e-83fd359be35f.mp4/index.m3u8",
+        "title": "기독교와세계문화_9주차_2",
     },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/43f51ef8-d547-4541-a3fa-9344b3437893/mp4/43f51ef8-d547-4541-a3fa-9344b3437893.mp4/index.m3u8",
-        "title": "재산거래와법_10주차",
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/d1dc0c48-8992-4c94-9652-241bcfe1731e/mp4/d1dc0c48-8992-4c94-9652-241bcfe1731e.mp4/index.m3u8",
+        "title": "기독교와세계문화_10주차_1",
     },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/e0731bca-e444-4a4d-bcdf-31b83c1eda80/mp4/e0731bca-e444-4a4d-bcdf-31b83c1eda80.mp4/index.m3u8",
-        "title": "재산거래와법_11주차",
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/9f3a36dc-351f-4b3b-86c9-8ffd8996cd75/mp4/9f3a36dc-351f-4b3b-86c9-8ffd8996cd75.mp4/index.m3u8",
+        "title": "기독교와세계문화_10주차_2",
     },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/c774f06b-c601-4958-b9a0-e28a4181ad65/mp4/c774f06b-c601-4958-b9a0-e28a4181ad65.mp4/index.m3u8",
-        "title": "재산거래와법_12주차",
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/b4ffcba6-f3e0-4c7e-9fd6-03cd89b07b59/mp4/b4ffcba6-f3e0-4c7e-9fd6-03cd89b07b59.mp4/index.m3u8",
+        "title": "기독교와세계문화_11주차_1",
     },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/f359949d-6372-4534-a2e3-142e41b39e58/mp4/f359949d-6372-4534-a2e3-142e41b39e58.mp4/index.m3u8",
-        "title": "재산거래와법_13주차",
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/07dd0932-4b92-4f72-972f-8c8e767c604a/mp4/07dd0932-4b92-4f72-972f-8c8e767c604a.mp4/index.m3u8",
+        "title": "기독교와세계문화_11주차_2",
     },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/ca0834bc-5df8-4e57-a542-fe5223b7185d/mp4/ca0834bc-5df8-4e57-a542-fe5223b7185d.mp4/index.m3u8",
-        "title": "재산거래와법_14주차",
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/f8c9636b-3660-480e-a18b-33784181ef72/mp4/f8c9636b-3660-480e-a18b-33784181ef72.mp4/index.m3u8",
+        "title": "기독교와세계문화_12주차_1",
+    },
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/d6d18adf-2771-4646-96bd-9df93245315a/mp4/d6d18adf-2771-4646-96bd-9df93245315a.mp4/index.m3u8",
+        "title": "기독교와세계문화_12주차_2",
+    },
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/ffab1630-f18c-40d4-9a95-ade4e3d12271/mp4/ffab1630-f18c-40d4-9a95-ade4e3d12271.mp4/index.m3u8",
+        "title": "기독교와세계문화_12주차_3",
+    },
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/769eb26f-cc63-44af-ba11-eb262303fced/mp4/769eb26f-cc63-44af-ba11-eb262303fced.mp4/index.m3u8",
+        "title": "기독교와세계문화_13주차_1",
+    },
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/a74cca51-3079-4ccc-ab6a-b3c9e83da628/mp4/a74cca51-3079-4ccc-ab6a-b3c9e83da628.mp4/index.m3u8",
+        "title": "기독교와세계문화_13주차_2",
+    },
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/dc2556d4-3bed-4cb0-b40e-9c61e3c65bef/mp4/dc2556d4-3bed-4cb0-b40e-9c61e3c65bef.mp4/index.m3u8",
+        "title": "기독교와세계문화_14주차_1",
+    },
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/02c60642-3905-40f4-9cfe-cef762c10b72/mp4/02c60642-3905-40f4-9cfe-cef762c10b72.mp4/index.m3u8",
+        "title": "기독교와세계문화_14주차_2",
+    },
+    {
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/ac1c892b-fe3b-43b4-a2ea-7920aad0950f/mp4/ac1c892b-fe3b-43b4-a2ea-7920aad0950f.mp4/index.m3u8",
+        "title": "기독교와세계문화_14주차_3",
     },
 ]
 
+if __name__ == '__main__':
+    # ffmpeg 명령어를 리스트로 작성
+    # '-y' 옵션은 출력 파일이 이미 존재할 경우, 덮어쓰기를 허용하는 옵션
+    for i in range(len(video_data)):
+        print(video_data[i]["title"], "다운로드 중...")
+        cmd = [
+            "ffmpeg",
+            "-i", video_data[i]["url"],
+            "-c", "copy",
+            "-y",
+            "video/"+video_data[i]["title"]+".mp4"
+        ]
 
-# ffmpeg 명령어를 리스트로 작성
-# '-y' 옵션은 출력 파일이 이미 존재할 경우, 덮어쓰기를 허용하는 옵션
-for i in range(len(video_data)):
-    cmd = [
-        "ffmpeg",
-        "-i", video_data[i]["url"],
-        "-c", "copy",
-        "-y",
-        "video/"+video_data[i]["title"]+".mp4"
-    ]
+        # subprocess.run을 이용해 명령어 실행
+        result = subprocess.run(cmd, capture_output=True, text=True)
 
-    # subprocess.run을 이용해 명령어 실행
-    result = subprocess.run(cmd, capture_output=True, text=True)
-
-    if result.returncode == 0:
-        print("다운로드 및 변환이 완료되었습니다.")
-    else:
-        print("다운로드 실패:")
-        print(result.stderr)
+        if result.returncode == 0:
+            print(video_data[i]["title"], "다운로드 완료")
+        else:
+            print("다운로드 실패:")
+            print(result.stderr)
--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -1,35 +1,5 @@
 import subprocess
-
-video_data = [
-    {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/9a8bb5a4-b2b5-4722-9ede-8b7df826ce67/mp4/9a8bb5a4-b2b5-4722-9ede-8b7df826ce67.mp4/index.m3u8",
-        "title": "재산거래와법_7주차",
-    },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/9c46869c-3941-449b-b13d-2f8675cb47c3/mp4/9c46869c-3941-449b-b13d-2f8675cb47c3.mp4/index.m3u8",
-        "title": "재산거래와법_9주차",
-    },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/43f51ef8-d547-4541-a3fa-9344b3437893/mp4/43f51ef8-d547-4541-a3fa-9344b3437893.mp4/index.m3u8",
-        "title": "재산거래와법_10주차",
-    },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/e0731bca-e444-4a4d-bcdf-31b83c1eda80/mp4/e0731bca-e444-4a4d-bcdf-31b83c1eda80.mp4/index.m3u8",
-        "title": "재산거래와법_11주차",
-    },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/c774f06b-c601-4958-b9a0-e28a4181ad65/mp4/c774f06b-c601-4958-b9a0-e28a4181ad65.mp4/index.m3u8",
-        "title": "재산거래와법_12주차",
-    },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/f359949d-6372-4534-a2e3-142e41b39e58/mp4/f359949d-6372-4534-a2e3-142e41b39e58.mp4/index.m3u8",
-        "title": "재산거래와법_13주차",
-    },
-{
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/ca0834bc-5df8-4e57-a542-fe5223b7185d/mp4/ca0834bc-5df8-4e57-a542-fe5223b7185d.mp4/index.m3u8",
-        "title": "재산거래와법_14주차",
-    },
-]
+from main import video_data
 
 for i in range(len(video_data)):
     cmd = [
