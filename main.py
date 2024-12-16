52c16ea4c52d0133af6c91ff665442eb0bb52d03
jhyunwoo <jhyunwoo0228@gmail.com>
2024-12-16 14:46:11 +0900

    end checkseogu

--- main.py
+++ main.py
@@ -2,60 +2,40 @@ import subprocess
 
 video_data = [
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/f3c04906-4ac7-419e-898b-cbec3940d5fb/mp4/f3c04906-4ac7-419e-898b-cbec3940d5fb.mp4/index.m3u8",
-        "title": "기독교와세계문화_9주차_1",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/d8c3b3d6-3155-4f45-98b1-13e89e78d024/mp4/d8c3b3d6-3155-4f45-98b1-13e89e78d024.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_역사적배경(4)",
     },
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/5dbeb766-8337-42a9-9d9e-83fd359be35f/mp4/5dbeb766-8337-42a9-9d9e-83fd359be35f.mp4/index.m3u8",
-        "title": "기독교와세계문화_9주차_2",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/b980d796-11e5-4f03-b598-7879ff5c7f98/mp4/b980d796-11e5-4f03-b598-7879ff5c7f98.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_7장_인쇄기술의발전(1)",
     },
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/d1dc0c48-8992-4c94-9652-241bcfe1731e/mp4/d1dc0c48-8992-4c94-9652-241bcfe1731e.mp4/index.m3u8",
-        "title": "기독교와세계문화_10주차_1",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/74317fcf-a770-46cb-a4fc-94ede0c2287d/mp4/74317fcf-a770-46cb-a4fc-94ede0c2287d.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_7장_인쇄기술의발전(2)",
     },
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/9f3a36dc-351f-4b3b-86c9-8ffd8996cd75/mp4/9f3a36dc-351f-4b3b-86c9-8ffd8996cd75.mp4/index.m3u8",
-        "title": "기독교와세계문화_10주차_2",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/3ac7408d-85dd-43b6-9668-d44b6c88f501/mp4/3ac7408d-85dd-43b6-9668-d44b6c88f501.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_역사적배경(5)",
     },
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/b4ffcba6-f3e0-4c7e-9fd6-03cd89b07b59/mp4/b4ffcba6-f3e0-4c7e-9fd6-03cd89b07b59.mp4/index.m3u8",
-        "title": "기독교와세계문화_11주차_1",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/8411419f-fe19-4a68-8e55-0c5613aeeff4/mp4/8411419f-fe19-4a68-8e55-0c5613aeeff4.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_제7장_인쇄기술의발전(3)",
     },
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/07dd0932-4b92-4f72-972f-8c8e767c604a/mp4/07dd0932-4b92-4f72-972f-8c8e767c604a.mp4/index.m3u8",
-        "title": "기독교와세계문화_11주차_2",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/dc8fac54-cb68-45d0-b4c8-f7b72c4c62ad/mp4/dc8fac54-cb68-45d0-b4c8-f7b72c4c62ad.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_제11장_산업혁명시대의책문화(1)",
     },
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/f8c9636b-3660-480e-a18b-33784181ef72/mp4/f8c9636b-3660-480e-a18b-33784181ef72.mp4/index.m3u8",
-        "title": "기독교와세계문화_12주차_1",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/6dc0ef4f-5aa3-495c-a20e-f2e31966f75a/mp4/6dc0ef4f-5aa3-495c-a20e-f2e31966f75a.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_제11장_산업혁명시대의책문화(2)",
     },
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/d6d18adf-2771-4646-96bd-9df93245315a/mp4/d6d18adf-2771-4646-96bd-9df93245315a.mp4/index.m3u8",
-        "title": "기독교와세계문화_12주차_2",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/8ad531ff-b4e0-4d5e-80d2-c34ea25dcde0/mp4/8ad531ff-b4e0-4d5e-80d2-c34ea25dcde0.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_제11장_산업혁명시대의책문화(3)",
     },
     {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/ffab1630-f18c-40d4-9a95-ade4e3d12271/mp4/ffab1630-f18c-40d4-9a95-ade4e3d12271.mp4/index.m3u8",
-        "title": "기독교와세계문화_12주차_3",
-    },
-    {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/769eb26f-cc63-44af-ba11-eb262303fced/mp4/769eb26f-cc63-44af-ba11-eb262303fced.mp4/index.m3u8",
-        "title": "기독교와세계문화_13주차_1",
-    },
-    {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/a74cca51-3079-4ccc-ab6a-b3c9e83da628/mp4/a74cca51-3079-4ccc-ab6a-b3c9e83da628.mp4/index.m3u8",
-        "title": "기독교와세계문화_13주차_2",
-    },
-    {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/dc2556d4-3bed-4cb0-b40e-9c61e3c65bef/mp4/dc2556d4-3bed-4cb0-b40e-9c61e3c65bef.mp4/index.m3u8",
-        "title": "기독교와세계문화_14주차_1",
-    },
-    {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/02c60642-3905-40f4-9cfe-cef762c10b72/mp4/02c60642-3905-40f4-9cfe-cef762c10b72.mp4/index.m3u8",
-        "title": "기독교와세계문화_14주차_2",
-    },
-    {
-        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/ac1c892b-fe3b-43b4-a2ea-7920aad0950f/mp4/ac1c892b-fe3b-43b4-a2ea-7920aad0950f.mp4/index.m3u8",
-        "title": "기독교와세계문화_14주차_3",
+        "url": "https://qszhvydoggzp7663209.cdn.ntruss.com/hls/c36034aa-8830-4526-bfc2-4689a01d800a/mp4/c36034aa-8830-4526-bfc2-4689a01d800a.mp4/index.m3u8",
+        "title": "책의역사로본서구문명_역사적배경(5)-2",
     },
 ]
 
